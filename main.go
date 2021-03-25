73eba116d74f942dcc359f5f661c1804a926abc2
SeraphJACK <seraphjack@outlook.com>
2021-03-25 23:35:30 +0800

    Exit normally when receiving SIGINT or SIGTERM

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/SeraphJACK/go-annil/http"
 	"github.com/SeraphJACK/go-annil/storage"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -19,9 +21,15 @@ func main() {
 		_, _ = fmt.Fprintf(os.Stderr, "Failed to initialize database: %v", err)
 		os.Exit(1)
 	}
-	err = http.Init()
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "Failed to start http server: %v", err)
-		os.Exit(1)
-	}
+	go func() {
+		err = http.Init()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Failed to start http server: %v", err)
+			os.Exit(1)
+		}
+	}()
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	_ = <-sigChan
+	os.Exit(0)
 }
